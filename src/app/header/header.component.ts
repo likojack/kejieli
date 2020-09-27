b261d7c062999a9592598122a70b73af83814ef3
Michelle Liu <michelle.liu@hlaustralia.com.au>
2020-09-28 00:20:14 +0930

    various fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,10 @@
-import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  // encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
   public currentTab = 'home';
