5221a2ee0277fd788172597f0e03424b2ab631cb
MichelleLiuHL <michelle.liu@hlaustralia.com.au>
2021-09-11 01:04:06 +0930

    odam component barebone

--- src/app/Constants.ts
+++ src/app/Constants.ts
@@ -1,14 +1,17 @@
+  // tslint:disable:max-line-length
+
 export default class Constants {
-  public static IAN_REID = { name: 'Ian Reid', link: 'https://cs.adelaide.edu.au/~ianr/' };
+  public static KEJIE_LI = { name: 'Kejie Li', link: 'https://likojack.github.io/kejieli', institution: 'University Of Adelaide' };
+  public static IAN_REID = { name: 'Ian Reid', link: 'https://cs.adelaide.edu.au/~ianr/', institution: 'University Of Adelaide' };
   public static MARTIN_RUNZ = { name: 'Martin Rünz *', link: 'https://www.martinruenz.de/' };
   public static MENG_TANT = { name: 'Meng Tang', link: 'http://mengtang.org/' };
   public static LINGNI_MA = { name: 'Lingni Ma', link: 'https://scholar.google.nl/citations?user=eUAgpwkAAAAJ&hl=en' };
   public static CHEN_KONG = { name: 'Chen Kong', link: 'http://www.cs.cmu.edu/~chenk/' };
   public static TANNER_SCHMIDT = { name: 'Tanner Schmidt', link: 'https://tschmidt23.github.io/' };
   public static LOURDES_AGAPITO = { name: 'Lourdes Agapito', link: 'http://www0.cs.ucl.ac.uk/staff/L.Agapito/' };
-  public static JULIAN_STRAUB = { name: 'Julian Straub', link: 'http://people.csail.mit.edu/jstraub/' };
+  public static JULIAN_STRAUB = { name: 'Julian Straub', link: 'http://people.csail.mit.edu/jstraub/', institution: 'Facebook Reality Labs Research' };
   public static STEVEN_LOVEGROVE = { name: 'Steven Lovegrove', link: 'https://scholar.google.com/citations?user=JVum8voAAAAJ&hl=en' };
-  public static RICHARD_NEWCOMBE = { name: 'Richard Newcombe', link: 'https://scholar.google.co.uk/citations?user=MhowvPkAAAAJ&hl=en' };
+  public static RICHARD_NEWCOMBE = { name: 'Richard Newcombe', link: 'https://scholar.google.co.uk/citations?user=MhowvPkAAAAJ&hl=en', institution: 'Facebook Reality Labs Research' };
   public static RAVI_GARG = { name: 'Ravi Garg', link: 'https://scholar.google.com.au/citations?user=2dvDXjkAAAAJ&hl=en' };
   public static MING_CAI = { name: 'Ming Cai', link: 'https://www.roboticvision.org/rv_person/ming-cai/' };
   public static MEHDI_HOSSEINZADEH = { name: 'Mehdi Hosseinzadeh', link: 'https://www.roboticvision.org/rv_person/mehdi-hosseinzadeh/' };
@@ -16,11 +19,11 @@ export default class Constants {
   public static TRUNG_PHAM = { name: 'Trung Pham', link: 'http://trungtpham.github.io/' };
   public static HUANGYING_ZHAN = { name: 'Huangying Zhan', link: 'http://huangying-zhan.github.io/' };
   public static SAROJ = { name: 'Chamara Saroj Weerasekera', link: 'https://www.roboticvision.org/rv_person/saroj-weerasekera/' };
-  public static HAMID = { name: 'Hamid Rezatofighi', link: 'https://research.monash.edu/en/persons/hamid-rezatofighi/' };
-  public static DANIEL_DETONE = { name: 'Daniel DeTone', link: 'https://scholar.google.com/citations?user=FrQGs28AAAAJ&hl=en' };
-  public static STEVEN_CHEN = { name: 'Steven Chen', link: 'https://www.linkedin.com/in/steven-chen-a9baa562' };
-  public static MINH_VO = { name: 'Minh Vo', link: 'https://scholar.google.com/citations?user=H4g_IKkAAAAJ&hl=en' };
-  public static CHRIS_SWEENEY = { name: 'Chris Sweeney', link: 'https://scholar.google.com/citations?user=h-CpQGgAAAAJ&hl=en' };
+  public static HAMID = { name: 'Hamid Rezatofighi', link: 'https://research.monash.edu/en/persons/hamid-rezatofighi/', institution: 'Monash University' };
+  public static DANIEL_DETONE = { name: 'Daniel DeTone', link: 'https://scholar.google.com/citations?user=FrQGs28AAAAJ&hl=en', institution: 'Facebook Reality Labs Research' };
+  public static STEVEN_CHEN = { name: 'Steven Chen', link: 'https://www.linkedin.com/in/steven-chen-a9baa562', institution: 'Facebook Reality Labs Research' };
+  public static MINH_VO = { name: 'Minh Vo', link: 'https://scholar.google.com/citations?user=H4g_IKkAAAAJ&hl=en', institution: 'Facebook Reality Labs Research' };
+  public static CHRIS_SWEENEY = { name: 'Chris Sweeney', link: 'https://scholar.google.com/citations?user=h-CpQGgAAAAJ&hl=en', institution: 'Facebook Reality Labs Research' };
   public static HARSH_AGARWAL = { name: 'Harsh Agarwal', link: null };
 
 }
--- src/app/publications/odam/odam.component.ts
+++ src/app/publications/odam/odam.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import Constants from 'src/app/Constants';
+import { trigger, style, animate, transition } from '@angular/animations';
+import * as brands from '@fortawesome/free-brands-svg-icons';
+import * as solid from '@fortawesome/free-solid-svg-icons';
+import * as regular from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-odam',
   templateUrl: './odam.component.html',
-  styleUrls: ['./odam.component.scss']
+  styleUrls: ['./odam.component.scss'],
+  animations: [
+    trigger('slide', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateY(-15%)' }),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({ opacity: 1, transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class OdamComponent implements OnInit {
+  public faGithub = brands.faGithub;
+  public faScroll = solid.faScroll;
+  public faYoutube = brands.faYoutube;
 
+  public authors = [
+    Constants.KEJIE_LI, Constants.DANIEL_DETONE, Constants.STEVEN_CHEN, Constants.MINH_VO,
+    Constants.IAN_REID, Constants.HAMID, Constants.CHRIS_SWEENEY, Constants.JULIAN_STRAUB, Constants.RICHARD_NEWCOMBE
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public goToLink(url: string): void {
+    if (url) { window.open(url, '_blank'); }
+  }
 }
